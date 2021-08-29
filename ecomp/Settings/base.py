e6ee1887218301e339932ca0e66e722b347bf89f
Rahul <suryarising17@gmail.com>
2021-08-29 20:32:44 +0530

    remove

--- ecomp/Settings/base.py
+++ ecomp/Settings/base.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     # ! ADDING WHITENOISE MIDDLEWARE
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,4 +124,4 @@ MEDIA_URL='/media/'
 MEDIA_ROOT= BASE_DIR/'media' 
 
 # !WHITENOISE ADDTIONAL SETTIG FOR STORAGE 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
