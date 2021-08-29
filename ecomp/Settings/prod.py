ed8c18f137f6162ef6d070ccf5c7ec1ea540598f
Rahul <suryarising17@gmail.com>
2021-08-29 21:15:03 +0530

    allo

--- ecomp/Settings/prod.py
+++ ecomp/Settings/prod.py
@@ -5,7 +5,7 @@ with open(os.path.join(BASE_DIR,'secret.txt')) as f :
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['secaperah.herokuapp.com',]
+ALLOWED_HOSTS = ['secaperahwork.herokuapp.com',]
 
 INSTALLED_APPS += [
    'rest_framework',
